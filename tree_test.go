d07829e0f87c31b154db5d8492e4a91913eae0eb
嘟嘟喂杜杜 <754962942@qq.com>
2023-12-26 11:29:41 +0800

    fix/前缀树test

--- tree_test.go
+++ tree_test.go
@@ -14,10 +14,8 @@ func TestTreeNode_Put(t *testing.T) {
 	node.Put("/user/create/hello")
 	node.Put("/user/create/aaa")
 	node.Put("/users/create/bbb")
-
 	get := node.Get("/user/get/1")
 	fmt.Println(get)
-
 	get = node.Get("/user/create/hello")
 	fmt.Println(get)
 	get = node.Get("/user/create/aaa")
